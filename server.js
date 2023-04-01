abe19bc95a11a876d848dae8f30024740575b6c6
Your Name <JulienneJulien15@gmail.com>
2023-04-01 00:59:23 -0400

    updated package

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3004;
 
 // // TO TEST LIVE SERVER PORT ROUTE 
 // app.get("/", function (req, res) {
